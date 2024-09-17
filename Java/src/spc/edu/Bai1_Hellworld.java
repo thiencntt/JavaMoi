a91aefd7eaf414dc35591b25ffc0c17f9e9a854d
thiencntt <nguyenchithien@gmail.com>
2024-09-17 15:46:36 +0700

    sdfsdfsdf

---
+++ Java/src/spc/edu/Bai1_Hellworld.java
@@ -0,0 +1,17 @@
+package spc.edu;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class Bai1_Hellworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
